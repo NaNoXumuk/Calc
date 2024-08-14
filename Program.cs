8ed34c9c6ddc73a8a6a98b8b0900fa641fc93288
NaNoXumuk <famassamaf@gmail.com>
2024-08-14 15:17:32 +0900

    coding

--- Program.cs
+++ Program.cs
@@ -9,68 +9,15 @@ namespace Calc
     internal class Program
     {
         static void Main(string[] args)
-        {
-            float CacheResult=0;
-            int CountMathIterations = 0, i, DoPlusMinus = 0;
+        {           
+            int CountMathIterations = 0;
 
             Console.WriteLine("Введите пример для решения.");
             string Example = Console.ReadLine();
 
             CountMathIterations = GetCountMathIterations(Example);
-
-            while (CountMathIterations > 0)
-            {
-                
-                string[] Cache = Example.Split(' ');
-                for (i = -1; i <= Cache.Length - 1;)
-
-                {
-                    i = i + 2;
-                    if (i >= Cache.Length)
-                    {
-                        DoPlusMinus = 1;
-                        i = 1;
-                    }
-
-                    if ((Cache[i] != "+" && Cache[i] != "-") || (DoPlusMinus != 1))
-                        switch (Cache[i])
-                        {
-                            case "/":
-                            case "*":
-                                if (Cache[i] == "/")
-                                    CacheResult = Calculate('/', Cache[i - 1], Cache[i + 1]);                                
-                                else
-                                    CacheResult = Calculate('*', Cache[i - 1], Cache[i + 1]);
-                                Cache[i] = Convert.ToString(CacheResult);
-                                Cache[i - 1] = null;
-                                Cache[i + 1] = null;
-                                i = Cache.Length;
-                                break;
-                        }
-                    else
-                        switch (Cache[i])
-                        {
-                            case "+":
-                            case "-":
-                                if (Cache[i] == "+")
-                                    CacheResult = Calculate('+', Cache[i - 1], Cache[i + 1]); 
-                                else
-                                    CacheResult = Calculate('-', Cache[i - 1], Cache[i + 1]);
-                                Cache[i] = Convert.ToString(CacheResult);
-                                Cache[i - 1] = null;
-                                Cache[i + 1] = null;
-                                i = Cache.Length;
-                                break;
-                        }
-                  
-                }
-                CountMathIterations--;
-                Example = string.Empty;
-
-                for (i = 0; i < Cache.Length; i++)
-                    if (Cache[i] != null)
-                        Example = Example + Cache[i] + " ";
-            }
+            Example = StringToNumber(CountMathIterations, Example);
+           
             Console.WriteLine($"Ответ: {Example}");
             Console.ReadKey();
         }
@@ -95,6 +42,67 @@ namespace Calc
                     count++;
             return count;
         }
+
+        static string StringToNumber(int CountMathIterations, string Example)
+        {
+            float CacheResult = 0;
+            while (CountMathIterations > 0)
+            {
+                bool CanDoPlusMinus = false;
+                string[] Cache = Example.Split(' ');
+                for (int i = -1; i <= Cache.Length - 1;)
+
+                {
+                    i = i + 2;
+                    if (i >= Cache.Length)
+                    {
+                        CanDoPlusMinus = true;
+                        i = 1;
+                    }
+
+                    if ((Cache[i] != "+" && Cache[i] != "-") || (CanDoPlusMinus != true))
+                        switch (Cache[i])
+                        {
+                            case "/":
+                            case "*":
+                                if (Cache[i] == "/")
+                                    CacheResult = Calculate('/', Cache[i - 1], Cache[i + 1]);
+                                else
+                                    CacheResult = Calculate('*', Cache[i - 1], Cache[i + 1]);
+                                Cache[i] = Convert.ToString(CacheResult);
+                                Cache[i - 1] = null;
+                                Cache[i + 1] = null;
+                                i = Cache.Length;
+                                break;
+                        }
+                    else
+                        switch (Cache[i])
+                        {
+                            case "+":
+                            case "-":
+                                if (Cache[i] == "+")
+                                    CacheResult = Calculate('+', Cache[i - 1], Cache[i + 1]);
+                                else
+                                    CacheResult = Calculate('-', Cache[i - 1], Cache[i + 1]);
+                                Cache[i] = Convert.ToString(CacheResult);
+                                Cache[i - 1] = null;
+                                Cache[i + 1] = null;
+                                i = Cache.Length;
+                                break;
+                        }
+
+                }
+                CountMathIterations--;
+
+                Example = string.Empty;
+                for (int i = 0; i < Cache.Length; i++)
+                    if (Cache[i] != null)
+                        Example = Example + Cache[i] + " ";
+            }
+            return Example;
+        }
+
+        
 
     }
 }
